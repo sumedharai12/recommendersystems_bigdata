108b0f8448941b053691326e260eb88d58615ebc
sumedharai12 <51825250+sumedharai12@users.noreply.github.com>
2020-05-10 01:53:13 -0400

    New Version

--- read_data_nozero.py
+++ read_data_nozero.py
@@ -16,7 +16,7 @@ def read_data(spark):
     interactions = spark.sql('SELECT * FROM interactions WHERE rating!=0')
 
     # using 1% of total data
-    interactions = interactions.sample(False, 0.01, seed=1)
+    interactions = interactions.sample(False, 0.05, seed=1)
     interactions.createOrReplaceTempView('interactions')
     
     print("1% subsample of 'interactions' dataset taken")
@@ -32,7 +32,7 @@ def read_data(spark):
 
     print("Created subset of users with more than 10 interactions")
 
-    spark.sql('SELECT Count(Distinct(user_id)) FROM ss_df').show() # for 1% data: 10,482; for 10% data:266,086
+    spark.sql('SELECT Count(Distinct(user_id)) FROM ss_df').show() # for 5% data: 138,040; for 1% data: 10,482; for 10% data:266,086
 
     # Make a folder in dumbo hdfs -recom and save this file in parquet format
     ss_df.write.mode('overwrite').parquet("recom/ss_zero.parquet")
@@ -44,7 +44,7 @@ def read_data(spark):
 
     # making validation and test sets using 40% data on the new set - 106,436
 
-    subset_users = spark.sql("SELECT user_id FROM (SELECT Distinct(ss_df.user_id) FROM ss_df) ORDER BY RAND() LIMIT 4194") # made a limit for 40% of users
+    subset_users = spark.sql("SELECT user_id FROM (SELECT Distinct(ss_df.user_id) FROM ss_df) ORDER BY RAND() LIMIT 55216") # made a limit for 40% of users
     subset_users.createOrReplaceTempView('subset_users')
     subset_df = spark.sql('SELECT ss_df.* FROM ss_df INNER JOIN subset_users on ss_df.user_id=subset_users.user_id')
     # subset_df = temp_df[interactions.user_id.isin(val_users.user_id)]
@@ -61,8 +61,30 @@ def read_data(spark):
     holdout_df.createOrReplaceTempView('holdout_df')
     print("Half interactions of all 40% users captured")
 
+    # creating training dataset first
+    train_df = ss_df.subtract(holdout_df)
+    print("Train set created")
+    train_df.createOrReplaceTempView('train_df')
+
+    #removing books from val and test which are not present in the training set
+    m = spark.sql('''
+    SELECT DISTINCT(holdout_df.book_id) as book_id 
+    FROM holdout_df 
+    INNER JOIN train_df 
+    ON holdout_df.book_id=train_df.book_id
+    ''')
+
+    m.createOrReplaceTempView('m')
+
+    holdout_df = spark.sql('''
+    SELECT holdout_df.* 
+    FROM holdout_df
+    INNER JOIN m
+    ON holdout_df.book_id=m.book_id
+    ''')
+
     # getting a list of half of the unique users in holdout
-    half_users = spark.sql('SELECT DISTINCT(holdout_df.user_id) FROM holdout_df ORDER BY RAND() LIMIT 53218')
+    half_users = spark.sql('SELECT DISTINCT(holdout_df.user_id) FROM holdout_df ORDER BY RAND() LIMIT 27608')
     half_users.createOrReplaceTempView('half_users')
 
     val_df = spark.sql('SELECT holdout_df.* FROM holdout_df INNER JOIN half_users on holdout_df.user_id=half_users.user_id')
@@ -71,10 +93,8 @@ def read_data(spark):
     test_df = holdout_df.subtract(val_df)
     print("Test set created")
     test_df.createOrReplaceTempView('test_df')
-    train_df = ss_df.subtract(holdout_df)
-    print("Train set created")
-    train_df.createOrReplaceTempView('train_df')
     
+
     val_df.write.mode('overwrite').parquet('recom/val_zero.parquet')
     print("Validation set saved in 'recom' folder")
     test_df.write.mode('overwrite').parquet('recom/test_zero.parquet')
